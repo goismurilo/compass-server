d01f6ad0a88f5b2b8c1766516a43eceba3416c90
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-08-15 21:32:25 -0300

    add: Create Route View Files Profile

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,16 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
+import UploadConfig from './config/upload';
+
 import './database';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/files', express.static(UploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
