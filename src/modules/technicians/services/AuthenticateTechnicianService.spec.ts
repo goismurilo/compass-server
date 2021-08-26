4202fbd143d1ac252358c6fa87e2c626d2774793
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-08-26 01:20:47 -0300

    change: Test Jest Technician

--- src/modules/technicians/services/AuthenticateTechnicianService.spec.ts
+++ src/modules/technicians/services/AuthenticateTechnicianService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeTechniciansRepository from '../repositories/fakes/FakeTechniciansRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import CreateTechnicianService from './CreateTechnicianService';
@@ -19,7 +20,7 @@ describe('AuthenticateTechnicianService', () => {
             fakeHashProvider,
         );
 
-        await createTechnician.execute({
+        const technician = await createTechnician.execute({
             name: 'Paulo Cristovao',
             email: 'cristovao@gmail.com',
             password: 'cris123',
@@ -31,6 +32,51 @@ describe('AuthenticateTechnicianService', () => {
         });
 
         expect(response).toHaveProperty('token');
-        expect(response.technician).toHaveProperty('token');
+        expect(response.technician).toEqual(technician);
+    });
+
+    it('should not be able to authenticate with not existing technician', async () => {
+        const fakeTechniciansRepository = new FakeTechniciansRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        const authenticateTechnician = new AuthenticateTechnicianService(
+            fakeTechniciansRepository,
+            fakeHashProvider,
+        );
+
+        expect(
+            authenticateTechnician.execute({
+                email: 'cristovao@gmail.com',
+                password: 'cris123',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to authenticate with wrong password', async () => {
+        const fakeTechniciansRepository = new FakeTechniciansRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        const createTechnician = new CreateTechnicianService(
+            fakeTechniciansRepository,
+            fakeHashProvider,
+        );
+
+        const authenticateTechnician = new AuthenticateTechnicianService(
+            fakeTechniciansRepository,
+            fakeHashProvider,
+        );
+
+        await createTechnician.execute({
+            name: 'Paulo Cristovao',
+            email: 'cristovao@gmail.com',
+            password: 'cris123',
+        });
+
+        expect(
+            authenticateTechnician.execute({
+                email: 'cristovao@gmail.com',
+                password: '14421424',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
     });
 });
