42d3969cca583b3e6381115ba74a9e5365323a6e
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-09-04 23:55:30 -0300

    fix: Create Data

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -27,6 +27,8 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
         });
     }
 
+    console.log(err);
+
     return response.status(500).json({
         status: 'error',
         message: 'Internal Server Error',
