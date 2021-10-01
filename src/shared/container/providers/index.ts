7bf58cc49b1142fdef86f2189380fbe885ccddc5
Murilo Gois de Almeida <39069727+goismurilo@users.noreply.github.com>
2021-10-01 00:56:56 -0300

    fix: Change Inject Variable Instance

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -14,12 +14,12 @@ container.registerSingleton<IStorageProvider>(
     DiskStorageProvider,
 );
 
-container.registerInstance<IMailProvider>(
-    'MailProvider',
-    new EtherealMailProvider(),
-);
-
 container.registerInstance<IMailTemplateProvider>(
     'MailTemplateProvider',
     new HandlebarsMailTemplateProvider(),
+);
+
+container.registerInstance<IMailProvider>(
+    'MailProvider',
+    container.resolve(EtherealMailProvider),
 );
